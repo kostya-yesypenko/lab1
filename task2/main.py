82dec05f34df5ab275e917e3b4ff691aeaeb9b04
kiesypenko <kostaesipenko120@gmail.com>
2022-09-27 17:41:56 +0300

    Task2 update

--- task2/main.py
+++ task2/main.py
@@ -1,23 +1,22 @@
 import argparse
 import operator
+import math
+
+arg = argparse.ArgumentParser()
+arg.add_argument('operator', type=str)
+arg.add_argument('values', nargs="*", type=int)
+args = arg.parse_args()
+
+def main(oper, *num):
+    if hasattr(operator, oper):
+        calc = getattr(operator, oper)
+        return calc(*num)
+    elif hasattr(math, oper):
+        calc = getattr(operator, oper)
+        return calc(*num)
+    else:
+        print('Wrong input')
 
 
-def main():
-    # parse arguments
-    parser = argparse.ArgumentParser(description='doc')
-    parser.add_argument("function", help="Python function to run.")
-    parser.add_argument('integer1', metavar='N', type=int, help='an integer for the accumulator')
-    parser.add_argument('integer2', metavar='N', type=int, help='an integer for the accumulator')
-    args = parser.parse_args()
 
-    # try to get the function from the operator module
-    try:
-        func = getattr(operator, args.function)
-    except:
-        print('not correct function')
-
-    # run the function and pass in the args, print the output to stdout
-    print(func(args.integer1, args.integer2))
-
-
-main()
+print(main(args.operator, *args.values))
