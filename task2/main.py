7f926a938977c063ec7f42f335e7c44dc7e7a45c
kiesypenko <kostaesipenko120@gmail.com>
2022-10-06 10:59:51 +0300

    updated task2

--- task2/main.py
+++ task2/main.py
@@ -8,14 +8,17 @@ arg.add_argument('values', nargs="*", type=int)
 args = arg.parse_args()
 
 def main(oper, *num):
-    if hasattr(operator, oper):
-        calc = getattr(operator, oper)
-        return calc(*num)
-    elif hasattr(math, oper):
-        calc = getattr(operator, oper)
-        return calc(*num)
-    else:
-        print('Wrong input')
+    try:
+        if hasattr(operator, oper):
+            calc = getattr(operator, oper)
+            return calc(*num)
+        elif hasattr(math, oper):
+            calc = getattr(operator, oper)
+            return calc(*num)
+        else:
+            print('Wrong input')
+    except ZeroDivisionError:
+        pass
 
 
 
